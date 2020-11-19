846f8089d7dd03bb9826976d920f4b7cdc6c4812
hayato-nima <kdc98bac72xkx@gmail.com>
2020-11-20 05:14:05 +0900

    add:問題番号009
    問題番号009のSQL文を作成・実行し、実行後の画面をキャプチャしたファイルを添付しました

---
+++ db/db-challenge3/db-009/db-009.sql
@@ -0,0 +1,17 @@
+-- <実行内容>
+-- 以下の要件を満たした内容を出力すること
+-- ・各チャットルームの投稿数、チャットルーム名を表示すること
+-- ・「投稿数」という名前で投稿数を表示すること
+-- ・削除されているユーザ－の投稿、削除されている投稿は含まないこと
+-- ・投稿数の降順で表示
+
+SELECT count(*) as 投稿数, rooms.name AS チャットルーム名
+FROM (
+SELECT * FROM `messages` WHERE is_deleted=0
+) AS 論理削除抜きメッセージ表
+JOIN rooms
+on rooms.id = 論理削除抜きメッセージ表.room_id
+WHERE 論理削除抜きメッセージ表.post_user_id <> (SELECT id FROM `users` WHERE is_deleted=1)
+or 論理削除抜きメッセージ表.update_user_id <> (SELECT id FROM `users` WHERE is_deleted=1)
+GROUP by room_id
+ORDER BY 投稿数 DESC;
