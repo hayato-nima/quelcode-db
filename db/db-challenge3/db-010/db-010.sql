b8cc541bf3fc805e1f2740fca6b4205fe168e4d3
hayato-nima <kdc98bac72xkx@gmail.com>
2020-11-20 07:22:44 +0900

    add:問題番号010
    問題番号010のSQL文を作成・実行し、実行後の画面をキャプチャしたファイルを添付しました

---
+++ db/db-challenge3/db-010/db-010.sql
@@ -0,0 +1,19 @@
+-- <実行内容>
+-- 以下の要件を満たした内容を出力すること
+-- ・ユーザー名、投稿日時、チャットルーム名を表示すること
+-- ・各チャットルームの最新の投稿をしたユーザー名と投稿日時を表示すること
+-- ・削除されているユーザーの投稿、削除されている投稿は含まないこと
+-- ・チャットルームidの昇順で表示
+
+SELECT 
+削除済US.screen_name AS ユーザー名,
+削除済MS.updated_at AS 投稿日時 , 削除済RM.name AS チャットルーム名
+FROM  
+(SELECT * FROM `messages` WHERE is_deleted=0) AS 削除済MS
+JOIN 
+(SELECT * FROM `users` WHERE is_deleted=0) AS 削除済US
+on 削除済MS.update_user_id = 削除済US.id
+JOIN 
+(SELECT * FROM `rooms` WHERE is_deleted=0) AS 削除済RM
+on 削除済MS.room_id = 削除済RM.id
+ORDER BY 削除済RM.id ASC
