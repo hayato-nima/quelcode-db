fbf7881eba1b690d367f58cc0b5d90d7889c27d7
hayato-nima <kdc98bac72xkx@gmail.com>
2020-11-20 14:40:39 +0900

    update:軽微な修正
    一部空白の調整をしました

--- db/db-challenge3/db-008/db-008.sql
+++ db/db-challenge3/db-008/db-008.sql
@@ -5,12 +5,19 @@
 -- ・削除されているユーザー、削除されているチャットルームは表示しないこと
 -- ・参加日時の昇順で表示
 
-SELECT  users.screen_name AS ユーザー名,  rooms.name AS チャットルーム名,LEFT(entered_at,10) AS 参加日時
+SELECT  users.screen_name   AS ユーザー名
+       ,rooms.name          AS チャットルーム名
+       ,LEFT(entered_at,10) AS 参加日時
 FROM entry_records
 JOIN users
 ON entry_records.user_id = users.id
 JOIN rooms
 ON entry_records.room_id = rooms.id
-WHERE (user_id <>(SELECT id FROM `users` WHERE is_deleted=1)) 
-AND (room_id <>(SELECT id FROM `rooms` WHERE is_deleted=1))  
-ORDER BY entry_records.entered_at ASC;
+WHERE (user_id <>( 
+SELECT  id
+FROM `users`
+WHERE is_deleted=1)) AND (room_id <>( 
+SELECT  id
+FROM `rooms`
+WHERE is_deleted=1))
+ORDER BY entry_records.entered_at ASC; 
--- db/db-challenge3/db-010/db-010.sql
+++ db/db-challenge3/db-010/db-010.sql
@@ -5,15 +5,27 @@
 -- ・削除されているユーザーの投稿、削除されている投稿は含まないこと
 -- ・チャットルームidの昇順で表示
 
-SELECT 
-削除済US.screen_name AS ユーザー名,
-削除済MS.updated_at AS 投稿日時 , 削除済RM.name AS チャットルーム名
-FROM  
-(SELECT * FROM `messages` WHERE is_deleted=0) AS 削除済MS
+SELECT  削除済US.screen_name AS ユーザー名
+       ,削除済MS.updated_at  AS 投稿日時 
+       ,削除済RM.name        AS チャットルーム名
+FROM 
+(
+	SELECT  *
+	FROM `messages`
+	WHERE is_deleted=0 
+) AS 削除済MS
 JOIN 
-(SELECT * FROM `users` WHERE is_deleted=0) AS 削除済US
+(
+	SELECT  *
+	FROM `users`
+	WHERE is_deleted=0 
+) AS 削除済US
 ON 削除済MS.update_user_id = 削除済US.id
 JOIN 
-(SELECT * FROM `rooms` WHERE is_deleted=0) AS 削除済RM
+(
+	SELECT  *
+	FROM `rooms`
+	WHERE is_deleted=0 
+) AS 削除済RM
 ON 削除済MS.room_id = 削除済RM.id
 ORDER BY 削除済RM.id ASC
