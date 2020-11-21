da4bfe165b0ce484845655955e91182ed0f572d3
hayato-nima <kdc98bac72xkx@gmail.com>
2020-11-22 02:57:19 +0900

    update:SQL文の修正
    SQL文に誤りがあった為修正しました

--- db/db-challenge3/db-011/db-011.sql
+++ db/db-challenge3/db-011/db-011.sql
@@ -8,8 +8,7 @@
 
 BEGIN;
 UPDATE tasks
-SET is_completed_task=1, updated_at=NOW()
+SET is_completed_task=1, updated_at=NOW() ,update_user_id=1 
 WHERE is_deleted=0 
-AND update_user_id=1 
 AND (created_at BETWEEN '2020-04-26 09:00:00' AND '2020-04-26 11:30:00');
 COMMIT; 
